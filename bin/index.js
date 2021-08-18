d9499cfb7e1bc6e6c0e2c0f08e81a67c84586c3d
hieu.nguyen <hieu.nguyen@amela.vn>
2021-08-18 11:26:43 +0700

    Add: template

--- bin/index.js
+++ bin/index.js
@@ -1,188 +1,49 @@
 #!/usr/bin/env node
-const { prompt } = require("inquirer")
-const {
-  readdirSync,
-  statSync,
-  mkdirSync,
-  existsSync,
-  readFileSync,
-  writeFileSync,
-} = require("fs")
-const { join } = require("path")
-const { spawn, spawnSync } = require("child_process")
-const { render } = require("./utils/template")
-const { argv } = require("yargs")
-const chalk = require("chalk")
-const boxen = require("boxen")
+const { prompt } = require("inquirer");
+const { readdirSync } = require("fs");
+const { join } = require("path");
 
-const CURR_DIR = process.cwd()
-const CHOICES = readdirSync(join(__dirname, "templates"))
+const {
+  argvToObject,
+  createDirectoryContents,
+  createGitRepo,
+  createProject,
+  postProcess,
+  showMessage,
+} = require("./utils");
+
+const ARGV = argvToObject();
+const CURR_DIR = process.cwd();
+const CHOICES = readdirSync(join(__dirname, "templates"));
 const QUESTIONS = [
   {
     name: "template",
     type: "list",
     message: "What project template would you like to generate?",
     choices: CHOICES,
-    when: () => !argv["template"],
+    when: () => !ARGV.template,
   },
   {
     name: "name",
     type: "input",
     message: "Project name:",
-    when: () => !argv["name"],
+    when: () => !ARGV.name,
     validate: (input) => {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
       else
-        return "Project name may only include letters, numbers, underscores and hashes."
+        return "Project name may only include letters, numbers, underscores and hashes.";
     },
   },
-]
-
-const runCommand = (command, args) => {
-  return new Promise((resolve, reject) => {
-    const child = spawn(command, args, { stdio: "inherit" })
-    child.on("close", (code) => {
-      if (code !== 0) {
-        reject({
-          command: `${command} ${args.join(" ")}`,
-        })
-        return
-      }
-      resolve()
-    })
-  })
-}
-
-const createGitRepo = async () => {
-  try {
-    await runCommand("git", ["init"])
-    await runCommand("git", ["add", "."])
-    await runCommand("git", ["commit", "-m", '"Init project"'])
-    console.log(chalk.green(`New git repo initialized successfully!`))
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const showMessage = (options) => {
-  const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "green",
-  }
-
-  const starting = chalk.white.bold(
-    `Move to project directory:\n\n$ cd ${options.projectName}\n\nFollow this document to start: ${options.homepage}`,
-  )
-  console.log(boxen(starting, boxenOptions))
-  console.log(chalk.cyan.bold(`Happy Hacking! From ${options.author} with ❤️`))
-  console.log()
-}
-
-const createProject = (projectPath) => {
-  if (existsSync(projectPath)) {
-    console.log(
-      chalk.red(`Folder ${projectPath} exists. Delete or use another name.`),
-    )
-    return false
-  }
-
-  mkdirSync(projectPath)
-  return true
-}
-
-const isNode = (options) => {
-  return existsSync(join(options.templatePath, "package.json"))
-}
-
-const useYarn = () => {
-  const child = spawnSync("which", ["yarn"])
-  return child.status === 0
-}
-
-const useNpm = () => {
-  const child = spawnSync("which", ["npm"])
-  return child.status === 0
-}
-
-const postProcess = (options) => {
-  if (isNode(options)) {
-    return postProcessNode(options)
-  }
-  return true
-}
-
-const postProcessNode = async (options) => {
-  process.chdir(options.tartgetPath)
-
-  let cmd = ""
-  let args = []
-
-  if (useYarn()) {
-    cmd = "yarn"
-  } else if (useNpm) {
-    cmd = "npm"
-    args = ["i", "--save", "--no-audit", "--save-exac", "--loglevel", "error"]
-  }
-
-  if (cmd) {
-    try {
-      console.log()
-      console.log(`Installing dependencies...`)
-      await runCommand(cmd, args)
-      console.log(chalk.green(`Dependencies installed successfully!`))
-      console.log()
-    } catch (error) {
-      console.log(error)
-      return false
-    }
-  } else {
-    console.log(chalk.red("No yarn or npm found. Cannot run installation."))
-  }
-
-  return true
-}
-
-const createDirectoryContents = (templatePath, projectName) => {
-  const filesToCreate = readdirSync(templatePath)
-
-  filesToCreate.forEach((file) => {
-    const origFilePath = join(templatePath, file)
-
-    // get stats about the current file
-    const stats = statSync(origFilePath)
-
-    if (stats.isFile()) {
-      let contents = readFileSync(origFilePath, "utf8")
-
-      contents = render(contents, { projectName })
-
-      if (file === 'gitignore') file = '.gitignore';
-
-      const writePath = join(CURR_DIR, projectName, file)
-      writeFileSync(writePath, contents, "utf8")
-    } else if (stats.isDirectory()) {
-      mkdirSync(join(CURR_DIR, projectName, file))
-
-      // recursive call
-      createDirectoryContents(
-        join(templatePath, file),
-        join(projectName, file),
-      )
-    }
-  })
-}
+];
 
 prompt(QUESTIONS).then(async (answers) => {
-  answers = Object.assign({}, answers, argv)
+  answers = Object.assign({}, answers, ARGV);
 
-  const projectChoice = answers["template"]
-  const projectName = answers["name"]
-  const templatePath = join(__dirname, "templates", projectChoice)
-  const tartgetPath = join(CURR_DIR, projectName)
-  const author = "Hieu Nguyen"
-  const homepage = "https://github.com/hieubeo0/create-test-app/blob/master/README.md"
+  const projectChoice = answers.template;
+  const projectName = answers.name;
+  const templatePath = join(__dirname, "templates", projectChoice);
+  const tartgetPath = join(CURR_DIR, projectName);
+  const author = "Hieu Nguyen";
 
   const options = {
     projectName,
@@ -190,20 +51,17 @@ prompt(QUESTIONS).then(async (answers) => {
     templatePath,
     tartgetPath,
     author,
-    homepage
-  }
+  };
 
   if (!createProject(tartgetPath)) {
-    return
+    return;
   }
 
-  createDirectoryContents(templatePath, projectName)
+  createDirectoryContents(templatePath, projectName, author, CURR_DIR);
 
-  if (!(await postProcess(options))) {
-    return
-  }
+  await postProcess(options)
 
   await createGitRepo()
 
-  showMessage(options)
-})
+  showMessage(options);
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Start a project with code base
+
+## Quick start
+
+```bash
+$ npx create-base-test
+
+or
+
+$ npx create-base-test --name=<project name> --template=<template name>
+```
+
+#### Options
+
+- --name: Name of the project you want to create
+- --template: Template of the project you want to create<br/>
+  template names: [nestjs-code-base | nextjs-code-base]
+
+## Documents
+
+- [NestJs code base](https://github.com/hieubeo0/create-test-app/tree/master/bin/templates/nestjs-code-base#readme)
+- [NextJs code base](https://github.com/hieubeo0/create-test-app/tree/master/bin/templates/nextjs-code-base#readme)
